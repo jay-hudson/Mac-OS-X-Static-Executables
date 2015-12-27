a5f26e860011aabd2e43b01c86d24dc440196b75
jay-hudson <jkh30003@yahoo.com>
2015-12-26 19:38:06 -0500

    remove comments on as 32

--- static.s
+++ static.s
@@ -27,36 +27,3 @@ _start:
     movl $1, %eax
     movl $0, %ebx
     int $0x80
-
-#     .globl _staticmsg
-#     .data
-#     .align 2
-# _staticmsg:
-#     .long   40 # string plus newline
-  
-#   strlength:
-#    $40           # string length in bytes
-
-# .text
-# .globl _entrypoint               # Program's entry point
-# _entrypoint:
-#     push ebp
-#     push esp, ebp
-#     push $strlength        # message length                           
-#     push $staticmsg        # message to write
-#     push $0x01             # 0x01 refers to stdout
-
-#     # create the syscall
-#     mov $0x04, eax               # 0x04 is the write syscall 
-#     sub $0x04, esp               # OS X need a bit of padding on the stack
-#     int 0x80                    # This actually executed the syscall
-
-#     add $0x10, esp               # Pushed three values, plus the pad to stack
-
-#     # exit the program
-#     push 0x0              # Equivalent to return 0 in C
-#     mov $0x01, eax               # 0x01 is the syscall number for exit 
-#     sub $0x04, esp               # OS X (and BSD) system calls needs "extra space" on stack
-#     int 0x80                    # make the syscall
-
-#   s
